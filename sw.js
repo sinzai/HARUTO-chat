fe952f16eff402fd99030485f44d1b253390f3de
sinzai <273777717+sinzai@users.noreply.github.com>
2026-06-25 19:50:48 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ const urlsToCache = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
